f59570121d9f3d99caacfd9ebbbfc4c4fba3a5b0
Lahav Pelts <lahavp09@gmail.com>
2025-07-17 21:49:53 +0300

    ex8

--- Main.java
+++ Main.java
@@ -4,6 +4,7 @@ import java.awt.Color;
 import src.codes.AnimationTimed;
 import src.codes.BlinkAnimation;
 import src.codes.MostBeautifulAnimation;
+import src.codes.SequentialAnimationGroup;
 import src.codes.LedController;
 
 public class Main {
@@ -13,11 +14,16 @@ public class Main {
         LedController LC = new LedController(ledSim);
 
         BlinkAnimation blinkAnimation = new BlinkAnimation(new Color(100, 0, 40), 5);
-
         MostBeautifulAnimation coolAnimation = new MostBeautifulAnimation(new Color(0, 100, 40), 5);
+
         LC.setAnimation(new AnimationTimed(blinkAnimation, 10.0));
-        BlinkAnimation defaultBlinkAnimation = new BlinkAnimation(new Color(0, 100, 40), 5);
         LC.setDefaultAnimation(coolAnimation);
+
+        SequentialAnimationGroup sequentialAnimationGroup = new SequentialAnimationGroup(
+            blinkAnimation, coolAnimation, blinkAnimation, coolAnimation
+        );
+
+        LC.setAnimation(sequentialAnimationGroup);
 
         while (true)
             LC.periodic();
