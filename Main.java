01c4d0899f13fc12a80bc5eeefc8278bf489d54c
Lahav Pelts <lahavp09@gmail.com>
2025-07-17 20:57:05 +0300

    ex6

--- Main.java
+++ Main.java
@@ -11,7 +11,9 @@ public class Main {
         LedSim ledSim = LedSim.getRowsSim(100);
         LedController LC = new LedController(ledSim);
         BlinkAnimation blinkAnimation = new BlinkAnimation(new Color(100, 0, 40), 5);
-        LC.setAnimation(new AnimationTimed(blinkAnimation, 15.0));
+        LC.setAnimation(new AnimationTimed(blinkAnimation, 10.0));
+        BlinkAnimation defaultBlinkAnimation = new BlinkAnimation(new Color(0, 100, 40), 5);
+        LC.setDefaultAnimation(defaultBlinkAnimation);
 
         while (true)
             LC.periodic();
